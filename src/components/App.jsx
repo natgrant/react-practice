dc7d090e0ab56bd27292d56bc999b01cd3683268
Nat Grant <email.natgrant@gmail.com>
2019-04-02 17:06:56 +1300

    passing props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import Posts from "./Posts"
+import faker from 'faker'
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Posts />
+        <Posts img={faker.image.avatar()} author={faker.name.firstName()} timeAgo="6:30PM" post={faker.lorem.sentence()} />
+        <Posts img={faker.image.avatar()} author={faker.name.firstName()} timeAgo="6:30PM" post={faker.lorem.sentence()} />
+        <Posts img={faker.image.avatar()} author={faker.name.firstName()} timeAgo="6:30PM" post={faker.lorem.sentence()} />
       </Fragment>
     );
   }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import faker from 'faker'
 
-class Posts extends Component {
-  render() {
-    return (
-      <div className="ui container comments">
-        <div className="comment">
-          <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()}></img>
-          </a>
-          <div className="content"><a href="/" className="author">Sam</a>
-            <div className="metadata">
-              <span className='date'>Today at 6:00PM</span>
-            </div>
-            <div className="text">Sweet Blog Post</div>
+const Posts = (props) => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={props.img}></img>
+        </a>
+        <div className="content"><a href="/" className="author">{props.author}</a>
+          <div className="metadata">
+            <span className='date'>{props.timeAgo}</span>
           </div>
+          <div className="text">{props.post}</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default Posts;
